0ff2d554682a586ea29aed993bc75119644003c3
Raphael Pinheiro <70273577+raphael-ps@users.noreply.github.com>
2024-12-09 17:44:29 -0300

    Teste Inicial
    Fazendo requisições

---
+++ src/app/url-service.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UrlServiceService {
+
+  constructor(private readonly httpClient : HttpClient) { }
+
+  addShortUrl = (info: {
+    originalUrl : string;
+    nickname : string;
+  }) => {
+    console.log(info)
+    return this.httpClient.post("http://localhost:8080/shorten", info).subscribe({
+      next: (response) => {
+        console.log("Requisição bem-sucedida", response);
+      },
+      error: (err) => {
+        console.error("Erro na requisição:", err);
+      },
+      complete: () => {
+        console.log("Requisição completada!");
+      },
+    });
+  }
+
+  redirectToUrl = (nickname : string) => {
+    return this.httpClient.get("http://localhost:8080/"+nickname, {responseType: "text"}).subscribe({
+      next: (response) => {
+        console.log(response)
+        window.location.href = response
+      },
+      error: (err) => {
+        if (err.status == 404){
+          alert("Error 404 - Link Não Encontrado!\nVai Se Fuder! 🤬")
+        }
+      }
+    })
+  }
+}
